80120b7f09aa8856fb601fd088320309f055a279
Madhavbansal1 <madhavbansal6633@gmail.com>
2025-04-18 11:24:27 +0530

    Ques1

---
+++ SocialMediaMicroServices/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080")
+})
